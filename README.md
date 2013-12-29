d1c5676b35f1246a0f27dd1b6d368d2b7e7604cd
Vo Minh Thu <noteed@gmail.com>
2013-12-29 21:34:30 +0100

    Recognize when a /data volume is provided.

--- README.md
+++ README.md
@@ -14,3 +14,22 @@ directly starts a `psql` process within the container.
 
     > docker run -d pgserver run
     > docker run -t -i pgserver psql
+
+To keep the PostgreSQL cluster around, and possibly reuse it in the next run of
+the image, a volume called `/data` can be given to the container, e.g.:
+
+    > docker run -t -i -v `pwd`/data:/data pgserver psql
+
+When the container sees a `/data` directory, it will use it instead of the
+cluster that already exists within the image.
+
+One possible way to create initially the local `data` directory is to use
+`docker cp`, e.g.:
+
+    > docker cp a62681994e1f:/var/lib/postgresql/9.1/main .
+    > mv main data
+
+where `a62681994e1f` is the container ID of a previous run.
+
+    > docker run pgserver
+    > docker ps -l
--- pgserver/run.sh
+++ pgserver/run.sh
@@ -1,23 +1,38 @@
 #! /bin/bash
+
+# If no /data is provided, use the container cluster, otherwise /data is
+# assumed to be a valid PostgreSQL cluster.
+DATA=/var/lib/postgresql/9.1/main
+if [ -d "/data" ]; then
+  DATA=/data
+  chown postgres:postgres -R /data
+  chmod 0700 -R /data
+fi
+
 case $1 in
 psql)
   /bin/su postgres -c \
-    '/usr/lib/postgresql/9.1/bin/postgres \
-      -D /var/lib/postgresql/9.1/main \
-      -c config_file=/etc/postgresql/9.1/main/postgresql.conf &'
+    "/usr/lib/postgresql/9.1/bin/postgres \
+      -D $DATA \
+      -c config_file=/etc/postgresql/9.1/main/postgresql.conf &"
   sleep 5
   export PGPASSWORD=docker
   psql -h 127.0.0.1 -p 5432 -d docker -U docker
   ;;
 run)
   /bin/su postgres -c \
-    '/usr/lib/postgresql/9.1/bin/postgres \
-      -D /var/lib/postgresql/9.1/main \
-      -c config_file=/etc/postgresql/9.1/main/postgresql.conf'
+    "/usr/lib/postgresql/9.1/bin/postgres \
+      -D $DATA \
+      -c config_file=/etc/postgresql/9.1/main/postgresql.conf"
   ;;
 *)
   echo Commands:
   echo "  psql   Run the PostgreSQL server and a psql prompt."
   echo "  run    Run only the PostgreSQL server."
-  exit 0
 esac
+
+# Make /data useable again by any user on the host.
+# TODO Save the permissions and reset them instead.
+if [ -d "/data" ]; then
+  chmod 0777 -R /data
+fi
